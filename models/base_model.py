33c55c69e7ef9d0bce88aaac0bdb2afcbe64b791
DianaWangui <dianawanguikt048@gmail.com>
2024-03-04 18:19:35 +0300

    update the basemodel

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from uuid import uuid4
+from datetime import datetime
+class BaseModel:
+    id = str(uuid.uuid4)
+    created_at = now.datetime
+    updated_at = now.datetime
+
+    def __str__(self):
+        return [class.__name__] (self.id) <self.__dict__
